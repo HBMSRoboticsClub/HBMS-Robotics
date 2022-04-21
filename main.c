3edf378d96eccee10fc2b0a00a0250ece13a0b3e
HBMSRoboticsClub <103683123+HBMSRoboticsClub@users.noreply.github.com>
2022-04-21 14:12:29 -0400

    main.c

--- main.c
+++ main.c
@@ -2,42 +2,59 @@
 
 int main()
 {
-    wait_for_light(0);
+    
     enable_servos();
     set_servo_position(0, 1800);
     msleep(500);
     
-	motor(1,-90);
+    motor(1,-90);
     motor(0, 100);
-    msleep(2250);
+    msleep(2750);
   
-    motor(1,100);
+    motor(1,175);
     motor(0,100);
-    msleep(500);
+    msleep(700);
     
-    motor(1,-100);
-    motor(0, 110);
-    msleep(500);
     
-    motor(1, 100);
+ 
+    motor(1,-98);
     motor(0, 110);
-    msleep(285);
+    msleep(2750);
     
-    motor(1,-100);
+  
+    motor(1, -98);
     motor(0, 110);
-    msleep(8000);
+    msleep(2000);
+    
+ 
+    
+    motor(1, -98);
+    motor(0, 110);
+    msleep(3100);
  
     motor(1,-0);
     motor(0, 0);
-    msleep(3000);
+    msleep(1000);
     
     enable_servos();
-    set_servo_position(0, 850);
+    set_servo_position(0, 943);
     msleep(500);
     
     motor(1, 20);
     motor(0, 20);
-    msleep(6000);
+    msleep(7000);
+    
+    motor(1, -100);
+    motor(0, 110);
+    msleep(6500);
+    
+    enable_servos();
+    set_servo_position(0, 1800);
+    msleep(500);
+    
+    motor(1, 100);
+    motor(0, -110);
+    msleep(6500);
     
     
     ao();
