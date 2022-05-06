6d7b25d311bd9cdd49ce94892a1a53b3caebb225
HBMSRoboticsClub <103683123+HBMSRoboticsClub@users.noreply.github.com>
2022-05-06 17:13:54 -0400

    GetPomPoms
    Follow the line and get pom poms.

--- main.c
+++ main.c
@@ -1,32 +1,36 @@
 #include <kipr/wombat.h>
 
 int main()
-{
-
+	{
+	
+	wait_for_light(4);
+    	shut_down_in(119);
     	int ticks;
 	ticks = 0;
     	printf("Follow the line\n");
         enable_servos();
-	set_servo_position(0, 323);
+  	set_servo_position(0, 323);
     	msleep(500);
     
     	motor(1, -90);
    	motor(0, 100);
-        msleep(1750);
-    	while (ticks < 50000)
+        msleep(2200);
+    	while (ticks < 5400)
     	{
         	if (analog(0) > 3000)
         	{
-        	    	motor(0, 100);
+            		motor(0, 100);
             		motor(1, -100);
+                
         	}
         	else
         	{
             
-          	  	motor(0, 100);
+            		motor(0, 100);
             		motor(1, 80);
-                	ticks += 50;
+               
         	}
+                ticks += 1;
     	}
     
     	motor(0, 0);
@@ -42,26 +46,40 @@ int main()
         msleep(1500);
     
     	ticks = 0;
-    
- 	while (ticks < 48000)
+        
+    	while (ticks < 4300)
     	{
         	if (analog(0) > 3000)
         	{
             		motor(0, 100);
             		motor(1, -100);
+                
         	}
         	else
         	{
             
-        	    	motor(0, 100);
+            		motor(0, 100);
             		motor(1, 80);
-                	ticks += 50;
+               
         	}
+           	ticks += 1;
     	}
     
    	motor(0, -100);
+       	motor(1, -80);
+        msleep(650);
+    
+    	motor(0, 100);
+       	motor(1, -80);
+        msleep(650);
+    
+    	enable_servos();
+  	set_servo_position(0, 323);
+    	msleep(500);
+    
+    	motor(0, -100);
        	motor(1, 80);
-        msleep(750);
+        msleep(1050);
     
         	
     	ao();
